f49553c057640f7608d3fe949171e8ea444fcd78
nithisha007 <bnithisha7@gmail.com>
2021-01-05 15:07:28 +0530

    code outline

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 using namespace std;
 char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char player = 'X';
 void Draw()
 {
+    cout << "Tic Tac Toe v1.0" << endl;
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
@@ -12,9 +14,48 @@ void Draw()
 		cout << endl;
 	}
 }
+void Input()
+{
+    int a;
+	cout <<"Press the number of the field: ";
+	cin >> a;
+
+    
+	if (a == 1)
+        matrix[0][0] = player;
+    else if (a == 2)
+        matrix[0][0] = player;
+     else if (a == 3)
+        matrix[0][2] = player;
+    else if (a == 4)
+        matrix[1][0] = player;
+    else if (a == 5)
+        matrix[1][1] = player;
+    else if (a == 6)
+        matrix[1][2] = player;
+    else if (a == 7)
+        matrix[2][0] = player;
+    else if (a == 8)
+        matrix[2][1] = player;
+    else if (a == 9)
+        matrix[2][2] = player;
+}
+void TogglePlayer()
+{
+	if (player == 'X')
+		player = 'O';
+	else
+		player = 'X';
+}
 int main()
 {
     Draw();
+    while (1)
+	{
+		Input();
+		Draw();
+		TogglePlayer();
+	}
     system("pause");
     return 0;
 }
